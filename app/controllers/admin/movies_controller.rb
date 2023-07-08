4adbcd7ee0b426b87fc833aaea6bed9ac96b6524
mikiko.hashino <mikiko.hashino@enigmo.co.jp>
2023-07-08 23:07:07 +0900

    station04

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,4 +1,5 @@
 class Admin::MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
   def index
     @movies = Movie.all
       render :index
@@ -20,7 +21,25 @@ class Admin::MoviesController < ApplicationController
     end
   end
 
+  def edit
+    render :edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:success] = '映画が更新されました'
+      redirect_to admin_movies_path, status: :found
+    else
+      flash.now[:error] = '映画の更新に失敗しました'
+      render :edit
+    end
+  end
+
   private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
   def movie_params
     params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,5 @@ class ApplicationController < ActionController::Base
   def handle_error(exception)
     # エラーメッセージの処理やログ出力などのカスタム処理を行う
     flash.now[:error] = '予期しないエラーが発生しました'
-    render 'errors/internal_server_error', status: :internal_server_error
   end
 end
